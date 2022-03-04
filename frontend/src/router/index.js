be74912334ab7d78b00bc07d6aa09ede0c986d66
cwjchin <cyrilchin282@gmail.com>
2022-03-05 00:14:58 +0800

    Implemented sidebar

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,34 +2,39 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Registration from '../views/Registration.vue'
 import VerifyEmail from '../views/VerifyEmail.vue'
-import ConfirmRegist from '../views/ConfirmRegist.vue'
+import ConfirmRegist from '../views/ConfirmRegist'
+import TimeTable from '../views/TimeTable'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/registration',
-    name: 'registration',
-    component: Registration
-  },
-  {
-    path: '/verifyEmail',
-    name: 'verifyEmail',
-    component: VerifyEmail
-  },
-  {
-    path: '/confirmRegist',
-    name: 'confirmRegist',
-    component: ConfirmRegist
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/registration',
+        name: 'registration',
+        component: Registration
+    },
+    {
+        path: '/verifyEmail',
+        name: 'verifyEmail',
+        component: VerifyEmail
+    },
+    {
+        path: '/confirmRegist',
+        name: 'confirmRegist',
+        component: ConfirmRegist
+    },
+    {
+        path: '/timetable',
+        name: 'timeTable',
+        component: TimeTable
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
 
 export default router
---
+++ frontend/src/views/TimeTable.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="left">
+            <SideBar />
+        </div>
+        <div class="right">
+            <h1>Timetable Planner</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import SideBar from '../components/sidebar/SideBar'
+
+export default {
+  name: 'TimeTable',
+  components: {
+      SideBar
+  }
+};
+</script>
+
+<style scoped>
+    .container {
+        display: flex;
+        height: 100vh;
+        width: 100vw;
+        padding: 0;
+        max-width: unset;
+    }
+    .left {
+        flex: 2;
+        height: 100%;
+    }
+    .right {
+        flex: 10;
+    }
+
+    h1 {
+        margin: 3%;
+        margin-bottom: 1%;
+        text-align: left;
+    }
+</style>
