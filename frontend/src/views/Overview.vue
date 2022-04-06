be44384be2f1170b465b4702445fa4974af66cfe
meganmhl <meganhoilingng@gmail.com>
2022-04-06 23:43:01 +0800

    overview page with course add drop functions

--- frontend/src/views/Overview.vue
+++ frontend/src/views/Overview.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <Toast />
+        <ConfirmDialog :breakpoints="{ '960px': '75vw', '640px': '100vw' }" :style="{ width: '50vw' }"></ConfirmDialog>
         <UpperBar />
         <div class="container">
             <div class="left">
@@ -12,13 +14,7 @@
                         <Button class="p-button-text p-button-secondary block1">
                             <Card class="block2" @click="handleClick(course.code)">
                                 <template #title>
-                                    <div class="title">
-                                        {{ course.code }}
-                                        <div class="tool">
-                                            <Button icon="pi pi-ellipsis-v"
-                                                class="p-button-secondary p-button-text p-button-rounded" @click="handleDelete(course.code)"/>
-                                        </div>
-                                    </div>
+                                    <div class="title">{{ course.code }}</div>
                                 </template>
                                 <template #subtitle>
                                     <div style="overflow: auto;">{{ course.name }}</div>
@@ -27,28 +23,59 @@
                                     <div style="overflow: auto;">{{ course.professor }}</div>
                                 </template>
                             </Card>
+                            <div class="tool">
+                                <Button
+                                    icon="pi pi-ellipsis-v"
+                                    class="p-button-secondary p-button-text p-button-rounded"
+                                    @click="handleDelete(course.code)"
+                                />
+                            </div>
                         </Button>
                     </span>
-
                     <hr />
                     <h2>Other Courses</h2>
-                    <DataTable :value="courses" :paginator="true" :rows="5" data-key="id" :filters="filters" :selection="selected" selectionMode="single" @rowSelect="onRowSelect">
+                    <DataTable
+                        :value="courses"
+                        :paginator="true"
+                        :rows="5"
+                        data-key="id"
+                        :filters="filters"
+                        :selection="selected"
+                        selectionMode="single"
+                        @rowSelect="onRowSelect"
+                    >
                         <template #header>
                             <div class="p-input-icon-left" style="margin: 10px 0px;">
                                 <i class="pi pi-search" />
-                                <InputText type="text" v-model="filters['global'].value" placeholder="Search" />
+                                <InputText
+                                    type="text"
+                                    v-model="filters['global'].value"
+                                    placeholder="Search"
+                                />
                             </div>
                         </template>
                         <Column field="code" header="Course Code"></Column>
                         <Column field="name" header="Title" style="overflow: auto;" />
-                        <Column field="courseDescription" header="Description" style="overflow: auto; " />
+                        <Column
+                            field="courseDescription"
+                            header="Description"
+                            style="overflow: auto; "
+                        />
                         <Column style="width: 340px;">
                             <template #body="btn">
-                                <Button label="More Details" icon="pi pi-info"
+                                <Button
+                                    label="More Details"
+                                    icon="pi pi-info"
                                     class="p-button-rounded p-button-secondary p-button-raised"
-                                    style="margin: 0px 10px 0px 0px;" @click="handleClick(btn.data.code)"/>
-                                <Button label="Add Course" icon="pi pi-plus"
-                                    class="p-button-rounded p-button-warning p-button-raised" @click="addUserCourse(btn.data.code)"/>
+                                    style="margin: 0px 10px 0px 0px;"
+                                    @click="handleClick(btn.data.code)"
+                                />
+                                <Button
+                                    label="Add Course"
+                                    icon="pi pi-plus"
+                                    class="p-button-rounded p-button-warning p-button-raised"
+                                    @click="addUserCourse(btn.data.code)"
+                                />
                             </template>
                         </Column>
                     </DataTable>
@@ -62,6 +89,7 @@
 import SideBar from '../components/sidebar/CourseSideBar.vue';
 import { FilterMatchMode, FilterOperator } from 'primevue/api';
 import db from "../components/chatroom/firebase";
+
 import {
     ref,
     set,
@@ -74,47 +102,84 @@ export default {
         SideBar
     },
     methods: {
-            handleClick(code) {
-                let courseRow;
-                onValue(ref(db, "courses"), (snapshot) => {
-                    snapshot.forEach((childSnapshot) => {
-                        if (childSnapshot.val().code === code){
-                            courseRow = childSnapshot.val();
-                        }
-                    })
-                });
-                if (courseRow) {
-                    this.$router.push({ name: 'CourseReview', params: {code: code}} );
-                }
-            },
-            onResize () {
-                if (window.innerWidth <= 767) {
-                    this.isOnMobile = true
-                    this.collapsed = true
-                } else {
-                    this.isOnMobile = false
-                    this.collapsed = false
-                }
-            },
-            addUserCourse(code) {
-                onValue(ref(db, "usercourse"), (snapshot) => {
-                    snapshot.forEach((childSnapshot) => {
-                        if (childSnapshot.val().uid === localStorage.getItem('user')){
-                            console.log(childSnapshot.val().uid);
-                        }
-                    })
-                });
-            },
-            handleDelete(code) {
-                console.log(code);
+        handleClick(code) {
+            let courseRow;
+            onValue(ref(db, "courses"), (snapshot) => {
+                snapshot.forEach((childSnapshot) => {
+                    if (childSnapshot.val().code === code) {
+                        courseRow = childSnapshot.val();
+                    }
+                })
+            });
+            if (courseRow) {
+                this.$router.push({ name: 'CourseReview', params: { code: code } });
             }
         },
+        onResize() {
+            if (window.innerWidth <= 767) {
+                this.isOnMobile = true
+                this.collapsed = true
+            } else {
+                this.isOnMobile = false
+                this.collapsed = false
+            }
+        },
+        addUserCourse(code) {
+            var exist = "no";
+            onValue(ref(db, "usercourse"), (snapshot) => {
+                snapshot.forEach((childSnapshot) => {
+                    if (childSnapshot.val().uid == localStorage.getItem('user')) {
+                        if (childSnapshot.val().code === code) {
+                            exist = "yes";
+                        }
+                    }
+                })
+            });
+            if (exist == "no") {
+                let varAdd = { uid: localStorage.getItem('user'), code: code };
+                let userListRef = ref(db, "usercourse");
+                let newUserRef = push(userListRef);
+                set(newUserRef, varAdd);
+                window.alert(code + " added to your current course list");
+                window.location.reload();
+            } else {
+                window.alert(code + " already in your current course list");
+            };
+        },
+        handleDelete(code) {
+            this.$confirm.require({
+                message: 'Do you want to delete '+ code+' from your course list?',
+                header: 'Delete Confirmation',
+                icon: 'pi pi-info-circle',
+                acceptClass: 'p-button-danger',
+                accept: () => {
+                    let key = "tbc";
+                    onValue(ref(db, "usercourse"), (snapshot) => {
+                        snapshot.forEach((childSnapshot) => {
+                            if (childSnapshot.val().uid === localStorage.getItem('user')) {
+                                if (childSnapshot.val().code === code) {
+                                    key = childSnapshot.key;
+                                }
+                            }
+                        })
+                    });
+                    let refe = 'usercourse/' + key + '/'
+                    let userListRef = ref(db, refe);
+                    set(userListRef, null);
+                    this.$toast.add({ severity: 'info', summary: 'Confirmed', detail: 'Course deleted', life: 3000 });
+                },
+                reject: () => {
+                    this.$toast.add({ severity: 'error', summary: 'Rejected', detail: 'Request cancelled', life: 3000 });
+                }
+            });
+        },
+    },
     mounted() {
         onValue(ref(db, "usercourse"), (snapshot) => {
-            this.usercourse = {};
+            this.usercourse = [];
             snapshot.forEach((childSnapshot) => {
                 if (childSnapshot.val().uid === localStorage.getItem('user')) {
-                    this.usercourse = childSnapshot.val().code;
+                    this.usercourse.push(childSnapshot.val().code);
                 }
             })
         });
@@ -125,18 +190,18 @@ export default {
                     if (code == childSnapshot.val().code) {
                         this.usercourse_details.push(childSnapshot.val());
                     }
-                })
-            })
+                });
+            });
         });
         onValue(ref(db, "courses"), (snapshot) => {
             this.courses = [];
             snapshot.forEach((childSnapshot) => {
-                    this.courses.push(childSnapshot.val());
+                this.courses.push(childSnapshot.val());
             })
         });
         this.onResize();
         window.addEventListener('resize', this.onResize);
-        
+
     },
     data() {
         return {
@@ -165,8 +230,8 @@ export default {
 }
 .tool {
     position: absolute;
-    right: 0px;
-    top: 0px;
+    right: 20px;
+    top: 20px;
 }
 .content {
     padding: 50px;
