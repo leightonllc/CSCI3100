07a7f687b630b3cd546bfdc931b2a2c65dd1fe28
ethanwongys <100587181+ethanwongys@users.noreply.github.com>
2022-04-11 06:11:28 +0800

    all course 3 buttons (half-way)

--- frontend/src/views/ShowAllCourses.vue
+++ frontend/src/views/ShowAllCourses.vue
@@ -9,7 +9,7 @@
             </div>
             <div class="right">
                 <div>
-                    <span class="h2">Course List</span>
+                    <span class="h2">All Courses</span>
                     <div class="my-2 overflow-auto">
                     <p style="font-family:Segoe UI">
                         <DataTable
@@ -32,6 +32,7 @@
                                 />
                             </div>
                         </template>
+
                         <Column field="code" header="Course Code"></Column>
                         <Column field="name" header="Title" style="overflow: auto;" />
                         <Column field="courseDescription" header="Description" style="overflow: auto; ">
@@ -52,18 +53,64 @@
                             </template>
                         </Column>
                         <Column field="rating" header="Rating" style="overflow: auto;" />
+                        <Column style="overflow: auto;" header="Add">
+                            <template>
+                                <Button
+                                    icon="pi pi-plus"
+                                    class="p-button-rounded p-button-success p-button-raised"
+                                    style="margin: 0px 10px 0px 0px;"
+                                    @click="handleClick(course.code)"
+                                />
+                            </template>
+                        </Column>
+
+                        <Column style="overflow: auto" header="Delete">
+                            <template>
+                                <Button
+                                    icon="pi pi-minus"
+                                    class="p-button-rounded p-button-danger p-button-raised"
+                                    style="margin: 0px 10px 0px 0px;"
+                                    @click="handleDelete(course.code)"
+                                />
+                            </template>
+                        </Column>
+
+                        <Column style="overflow: auto" header="Edit">
+                            <template>
+                                <Button
+                                    icon="pi pi-sliders-h"
+                                    class="p-button-rounded p-button-secondary p-button-raised"
+                                    style="margin: 0px 10px 0px 0px;"
+                                    @click="editCourse(course.code)"
+                                />
+                            </template>
+                        </Column>
+
                     </DataTable>
                     </p>
                     </div>
                 </div>
             </div>
         </div>
+        <Dialog v-model:visible="courseDialog" :style="{width: '450px'}" header="Course Details" :modal="true" class="p-fluid">
+            <div class="field">
+                <label for="code">Course Code</label>
+                <InputText field="code"/>
+                <small class="p-error" v-if="submitted && !code.name">Name is required.</small>
+            </div>
+
+            <template #footer>
+                <Button label="Cancel" icon="pi pi-times" class="p-button-text" @click="hideDialog"/>
+                <Button label="Save" icon="pi pi-check" class="p-button-text" @click="saveCourse" />
+            </template>
+        </Dialog>
     </div>
 </template>
 
 <script>
+    import { collection, getDocs } from "firebase/firestore";
     import CourseSideBar from '../components/sidebar/CourseSideBar';
-import { FilterMatchMode, FilterOperator } from 'primevue/api';
+    import { FilterMatchMode, FilterOperator } from 'primevue/api';
     import db from "../components/chatroom/firebase";
     import {
         ref,
@@ -87,6 +134,8 @@ import { FilterMatchMode, FilterOperator } from 'primevue/api';
                     'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
                 },
                 selected:null,
+                submitted: false,
+                courseDialog: false,
             };
         },
         methods: {
@@ -105,6 +154,69 @@ import { FilterMatchMode, FilterOperator } from 'primevue/api';
                     localStorage.setItem('code', code);
                     localStorage.setItem('title', courseRow.name);
                     this.$router.push({ name: 'CourseReview', params: {code: code}} );
+                }
+            },
+            handleDelete(code) {
+                this.$confirm.require({
+                    message: 'Do you want to delete '+ code+' from your course list?',
+                    header: 'Delete Confirmation',
+                    icon: 'pi pi-info-circle',
+                    acceptClass: 'p-button-danger',
+                    accept: () => {
+                        let key = "tbc";
+                        onValue(ref(db, "usercourse"), (snapshot) => {
+                            snapshot.forEach((childSnapshot) => {
+                                if (childSnapshot.val().uid === localStorage.getItem('user')) {
+                                    if (childSnapshot.val().code === code) {
+                                        key = childSnapshot.key;
+                                    }
+                                }
+                            })
+                        });
+                        let refe = 'usercourse/' + key + '/'
+                        let userListRef = ref(db, refe);
+                        set(userListRef, null);
+                        this.$toast.add({ severity: 'info', summary: 'Confirmed', detail: 'Course deleted', life: 3000 });
+                        window.location.reload();
+                    },
+                    reject: () => {
+                        this.$toast.add({ severity: 'error', summary: 'Rejected', detail: 'Request cancelled', life: 3000 });
+                    }
+                });
+            },
+            editCourse(code) {
+                this.code = {...code};
+                this.courseDialog = true;
+            },
+            
+            hideDialog() {
+                this.courseDialog = false;
+                this.submitted = false;
+            },
+            saveCourse() {
+                this.submitted = true;
+
+                if (this.code.name.trim()) {
+                    var exist = "no";
+                    onValue(ref(db, "usercourse"), (snapshot) => {
+                        snapshot.forEach((childSnapshot) => {
+                            if (childSnapshot.val().uid == localStorage.getItem('user')) {
+                                if (childSnapshot.val().code === code) {
+                                    exist = "yes";
+                                }
+                            }
+                        })
+                    });
+                    if (exist == "no") {
+                        let varAdd = { uid: localStorage.getItem('user'), code: code };
+                        let userListRef = ref(db, "usercourse");
+                        let newUserRef = push(userListRef);
+                        set(newUserRef, varAdd);
+                        window.alert(code + " added to your current course list");
+                        window.location.reload();
+                    } else {
+                        window.alert(code + " already in your current course list");
+                    };
                 }
             },
             onResize () {
