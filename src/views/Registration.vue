a884cd5dc9eff8bc1f6c26c5793f786d87b1fad1
monkeyjai78 <76764676+monkeyjai78@users.noreply.github.com>
2022-05-06 23:50:36 +0800

    asd
    asd

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,3 +1,12 @@
+/**
+ * @Author: monkeyjai78
+ * @Description: /views/Registration.vue is the registration page for new user.
+ *              new user can fill in account name, email address and passowrd to create an account.
+ * @Date: 2022-05-06 19:58:42
+ * @Last Modified by:   meganmhl
+ * @Last Modified time: 2022-05-06 20:03:41
+ */
+
 <template>
   <div class="background">
     <div class="left">
@@ -5,20 +14,21 @@
     </div>
     <div class="right">
       <div class="login">
+        <!--account registration form-->
         <form class="form" @submit.stop.prevent="register()">
           <i class="pi pi-angle-left" onclick="history.back()" style="cursor: pointer;">Back</i>
           <div class="header">
-            <div class="loginStr1">Register Individual Account!</div>
-            <div class="loginStr2">
+            <div class="topic">Register Individual Account!</div>
+            <div class="content">
               For the purpose of industry regulation, your details are required.
             </div>
           </div>
           <div class="form-group">
-            <label for="exampleInputName1">Account Name</label>
+            <label for="InputName">Account Name</label>
             <input type="text" class="form-control" v-model="name" placeholder="Enter account name" />
           </div>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label for="InputEmail">Email address</label>
             <input type="email" class="form-control" required="true" v-model="email" placeholder="Email address" />
           </div>
           <div class="form-group">
@@ -43,43 +53,30 @@
 </template>
 
 <script>
-  import db from "../components/chatroom/firebase";
-  import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    sendEmailVerification,
-    updateProfile
-  } from "firebase/auth";
-  import {
-    ref,
-    set,
-    push,
-  } from "firebase/database";
-  import {
-      getStorage,
-      ref as rtdbref,
-      uploadBytes,
-      getDownloadURL
-  } from "firebase/storage";
-  import * as image from '../assets/defaultpropic.png'
-  const auth = getAuth();
-  const storage = getStorage();
-  export default {
-    name: "Registration",
-    data() {
-      return {
-        email: '',
-        password: '',
-        name: '',
-        image: image,
-      };
-    },
-   mounted() {
-     var x = new File(require('../assets/defaultpropic.png'))
-     console.log(x)
-   },
-    methods: {
-      register() {
+import db from "../components/chatroom/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile
+} from "firebase/auth";
+import {
+  ref,
+  set,
+  push,
+} from "firebase/database";
+export default {
+  name: "Registration",
+  data() {
+    return {
+      email: '',
+      password: '',
+      name: '',
+    };
+  },
+  methods: {
+    //create new user with email and password
+    register() {
         const auth = getAuth();
         console.log(this.email);
         console.log(this.password);
@@ -131,105 +128,90 @@
         
         }
       },
-      
-      //upload a new propic to the database
-      
-    },
-  };
+  },
+};
 </script>
 
 <style scoped>
-  .background {
-    min-height: 100%;
-    display: flex;
-  }
-
-  .left {
-    background: linear-gradient(0deg,
-        rgba(21, 101, 216, 0.7),
-        rgba(21, 101, 216, 0.7)),
-      url(~@/assets/images/cuhkbg.jpg);
-    flex: 2 2 0;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    font-weight: bold;
-    font-size: 16px;
-    color: #ffffff;
-    text-align: start;
-    padding: 20px;
-  }
-
-  .right {
-    flex: 3 3 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-
-  .form {
-    background: white;
-    padding: 40px 40px;
-    border-radius: 10px;
-    max-width: 500px;
-    text-align: start;
-  }
-
-  .header {
-    font-weight: bold;
-    font-size: 19px;
-    color: #a4a6b3;
-    padding: 0px 0px 20px 0px;
-  }
-
-  .loginStr1 {
-    font-weight: bold;
-    font-size: 30px;
-    color: #252733;
-  }
-
-  .loginStr2 {
-    font-weight: normal;
-    font-size: 18px;
-    color: #9fa2b4;
-  }
-
-  .form-group {
-    text-align: start;
-    font-weight: bold;
-    font-size: 16px;
-    color: #9fa2b4;
-    padding: 0px 0px 30px 0px;
-  }
-
-  .form-control {
-    padding: 10px;
-  }
-
-  .form-check {
-    font-weight: 500;
-    font-size: 16px;
-    align-items: center;
-    color: #696f79;
-    padding-bottom: 30px;
-  }
-
-  .button {
-    width: 90%;
-    padding: 15px 0px;
-  }
-
-  .signup {
-    font-size: 14px;
-    text-align: center;
-    color: #9fa2b4;
-    padding: 20px 0px;
-  }
-
-  .signuplink {
-    font-size: 14px;
-    font-weight: bold;
-    color: #1565d8;
-    text-decoration: none;
-  }
+.background {
+  min-height: 100%;
+  display: flex;
+}
+.left {
+  background: linear-gradient(0deg,
+      rgba(21, 101, 216, 0.7),
+      rgba(21, 101, 216, 0.7)),
+    url(~@/assets/images/cuhkbg.jpg);
+  flex: 2 2 0;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  font-weight: bold;
+  font-size: 16px;
+  color: #ffffff;
+  text-align: start;
+  padding: 20px;
+}
+.right {
+  flex: 3 3 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.form {
+  background: white;
+  padding: 40px 40px;
+  border-radius: 10px;
+  max-width: 500px;
+  text-align: start;
+}
+.header {
+  font-weight: bold;
+  font-size: 19px;
+  color: #a4a6b3;
+  padding: 0px 0px 20px 0px;
+}
+.topic {
+  font-weight: bold;
+  font-size: 30px;
+  color: #252733;
+}
+.content {
+  font-weight: normal;
+  font-size: 18px;
+  color: #9fa2b4;
+}
+.form-group {
+  text-align: start;
+  font-weight: bold;
+  font-size: 16px;
+  color: #9fa2b4;
+  padding: 0px 0px 30px 0px;
+}
+.form-control {
+  padding: 10px;
+}
+.form-check {
+  font-weight: 500;
+  font-size: 16px;
+  align-items: center;
+  color: #696f79;
+  padding-bottom: 30px;
+}
+.button {
+  width: 90%;
+  padding: 15px 0px;
+}
+.signup {
+  font-size: 14px;
+  text-align: center;
+  color: #9fa2b4;
+  padding: 20px 0px;
+}
+.signuplink {
+  font-size: 14px;
+  font-weight: bold;
+  color: #1565d8;
+  text-decoration: none;
+}
 </style>
